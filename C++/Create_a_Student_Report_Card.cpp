84ce2fe14e576b75dcef0f007d381f9ccea773f3
Keithron J. Valera Jr <ronaldjoaquinvalerajr@gmail.com>
2026-06-28 23:24:19 +0800

    adding a few more codes

---
+++ C++/Create_a_Student_Report_Card.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int studentID = 06255;
+  float score = 89.5;
+  char grade = 'S';
+
+  cout<<"The student ID is: "<<studentID<<endl;
+  cout<<"The score is: "<<score<<endl;
+  cout<<"The grade is: "<<grade<<endl;
+
+  return 0;
+}
--- C++/user_input.cpp
+++ C++/user_input.cpp
@@ -3,12 +3,27 @@ using namespace std;
 
 int main()
 {
+  /*
+  cout<<"This is how to use a user Input"<<endl;
   int x;
 
   cout<<"Type a Number: ";
   cin>>x;
 
   cout<<"Your number is: "<<x<<endl;
+
+  cout<<endl;
+  */
+
+  cout<<"This is a Simple calculor"<<endl;
+  cout<<endl;
+  int x, y, sum;
+  cout<<"Type a number: ";
+  cin>>x;
+  cout<<"Type another number: ";
+  cin>>y;
+  sum = x + y;
+  cout<<"The Sum is: "<<sum<<endl;
 
   return 0;
 }
