d8ce220c664f459d830d469216898268fd5d6403
Keithron J. Valera Jr <ronaldjoaquinvalerajr@gmail.com>
2026-06-29 08:32:07 +0800

    hello

---
+++ C++/operators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Operators are used to perform operations on variables and values"<<endl;
+  cout<<"C++ divides the operators into the following groups"<<endl;
+  cout<<"* Arithmetic operators \n* Assignment operators \n* Comparison operators \n* Logical operators \n* Bitwise operators"<<endl;
+
+  cout<<endl;
+
+  cout<<"==========Arithmetic Operators=========="<<endl<<endl;
+  cout<<" + Addition \nAdds together two values \n-> x + y"<<endl<<endl;
+  cout<<" - Subraction \nSubtracts one value from another \n-> x - y"<<endl<<endl;
+  cout<<" * Multiplication \nMultiplies two values \n-> x * y"<<endl<<endl;
+  cout<<" / Division \nDivides one value by another \n-> x / u"<<endl<<endl;
+  cout<<" % Modulus \nReturns the division remainder \n-> x % y"<<endl<<endl;
+  cout<<" ++ Increment \nIncreases the value of a variable by 1 \n-> ++x or x++"<<endl<<endl;
+  cout<<"-- Decrement \nDecreases the value of a variable by 1 \n-> --x or x--"<<endl<<endl;
+
+  return 0;
+}
