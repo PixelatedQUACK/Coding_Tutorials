34ae7c41c17bd3680c773bfdb2e0ee1df51711e6
Keithron J. Valera Jr <ronaldjoaquinvalerajr@gmail.com>
2026-06-29 08:57:19 +0800

    hey

---
+++ Python/hello_world.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+print("Have a good day")
+print("Learning Python is fun\n")
+
+print("If you want to print without a new line, you can use end=" "")
+print("Hello", end="")
+print(" World\n")
+
+print("You can also print numbers, however printing numbers doesnt require to be inside the quotes")
+print(3)
+print(5)
+
+print("\nYou can also do math inside the print function")
+print(5 + 3)
+print(1 + 2 + 3)
+
+print("\nMixed Text and Numbers")
+print("I am", 35, "Years old")
+
+print("\nComments")
+print("Comments are used to take note inside a python code")
+print("#this is a comment, for more details check the code")
+#this is a comment, it can only comment 1 line
+"""
+This is also a comment
+written in multiple lines,
+to do this you need to 3 quotation marks from the start and the end
+"""
