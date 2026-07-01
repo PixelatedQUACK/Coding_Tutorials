d83b3e1f86426ab14a91079d2c4bbc6cb08c82ed
Keithron J. Valera Jr <ronaldjoaquinvalerajr@gmail.com>
2026-07-01 10:59:22 +0800

    added list

--- Python/lists.py
+++ Python/lists.py
@@ -1,1 +1,17 @@
 print("Python Lists")
+print("This is used to store many data inside one variable\n")
+print('Thislist = ["Apple","Orrange","Banana"]')
+Thislist = ["Apple", "Orrange", "Banana"]
+print(Thislist)
+
+print("\nLists starts with 0")
+print('In the example above the start with the position 0 with the variable "Apple",\nthe second is position 1 with the variable "Orrange" \nand the third is position 2 with the variable "Banana"\n')
+print("To access a list with a specific data in mind, you can use list[] with the position of the data inside the bracket")
+print('An example of this is\n\nThislist = ["Apple","Orrange","Banana"]')
+print('"print(Thislist[0])"\n')
+print(Thislist[0])
+print('This outputs its data which is Apple')
+
+print("\nTo output the size of the list we can use len()\n")
+print("using len(Thislist) outputs 3 as its size")
+print(len(Thislist))
